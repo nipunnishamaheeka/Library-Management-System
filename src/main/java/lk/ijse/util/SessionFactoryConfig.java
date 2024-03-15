6d4d17093db078edffd54f9b29ea49ec5a99d821
nipunnishamaheeka <nishamaheeka123@gmail.com>
2024-03-15 11:53:36 +0530

    sessionFactory

--- src/main/java/lk/ijse/util/SessionFactoryConfig.java
+++ src/main/java/lk/ijse/util/SessionFactoryConfig.java
@@ -1,5 +1,9 @@
 package lk.ijse.util;
 
+import lk.ijse.entity.Book;
+import lk.ijse.entity.Branch;
+import lk.ijse.entity.Credentials;
+import lk.ijse.entity.Transactions;
 import lombok.SneakyThrows;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -18,7 +22,14 @@ public class SessionFactoryConfig {
 
     private SessionFactoryConfig(){
         StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().loadProperties("hibernate.properties").build();
-        Metadata metadata = new MetadataSources(serviceRegistry).getMetadataBuilder().build();
+
+        Metadata metadata = new MetadataSources(serviceRegistry)
+                .addAnnotatedClass(Book.class)
+                .addAnnotatedClass(Credentials.class)
+                .addAnnotatedClass(Branch.class)
+                .addAnnotatedClass(Transactions.class)
+                .getMetadataBuilder().build();
+
         sessionFactory = metadata.buildSessionFactory();
     }
 
