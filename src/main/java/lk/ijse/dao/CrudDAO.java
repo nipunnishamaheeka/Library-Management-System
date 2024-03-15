e108b19d8c73e90560838d6413afae308e18debb
nipunnishamaheeka <nishamaheeka123@gmail.com>
2024-03-15 20:49:47 +0530

    crud dao

--- src/main/java/lk/ijse/dao/CrudDAO.java
+++ src/main/java/lk/ijse/dao/CrudDAO.java
@@ -1,16 +1,14 @@
 package lk.ijse.dao;
 
-import lk.ijse.entity.Book;
-
 import java.util.List;
 
 public interface CrudDAO<T> extends SuperDAO{
     boolean save(T dto) throws Exception;
 
-    boolean update(int id,T dto) throws Exception;
+    boolean update(int id, T dto) throws Exception;
 
     boolean delete(int id) throws Exception;
     T search(int id) throws Exception;
 
-    List<Book> loadAll() throws Exception;
+    List<T> loadAll() throws Exception;
 }
